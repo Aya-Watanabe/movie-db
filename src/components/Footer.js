893fd6c80391de096fdcc68e0cfd85d399c4c998
Aya-Watanabe <Aya-Watanabe@users.noreply.github.com>
2020-12-11 03:09:44 -0800

    tmdb image added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,15 +8,13 @@ export const Footer = () => {
     return (
        
     <footer> 
-        <div class="">
-        This is footer
+        <div class="tmdb-logo">
+        <img src ={`https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg`} alt="Logo" style={{height:"30px"}} />
         </div>
         <div class="">
         This is footer
         </div>
-        <div class="">
-        This is footer
-        </div>
+        
     </footer> 
         
     )
