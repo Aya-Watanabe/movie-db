6e896a95a3c59aa8ccfe403dca170f04efe4ba45
Aya-Watanabe <Aya-Watanabe@users.noreply.github.com>
2020-12-11 02:00:49 -0800

    fontawsome cdn added

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
-export const Movie = ({title, poster_path, overview, vote_average}) => {
+export const Movie = ({title, poster_path, overview, vote_average, release_date}) => {
   
    
     return (
@@ -11,11 +11,14 @@ export const Movie = ({title, poster_path, overview, vote_average}) => {
             <img src={IMG_API + poster_path} alt={title}/>
             <div className="movie-info">
                 <h3>{title}</h3>
-                <span>{vote_average}</span>
+                <p>{release_date}</p>
+                <p>{vote_average}</p>
+              
             </div>
             <div className="movie-overlay">
                 <h2>Overview:</h2>
                 <p>{overview}</p>
+                <button className="add-btn">Add<i class="far fa-heart"></i></button>
             </div>
             
         </div>
