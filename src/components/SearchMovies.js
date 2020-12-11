97ed3fe43b0e8b64909c9c90dc42f15ca9f5c4f7
Aya-Watanabe <Aya-Watanabe@users.noreply.github.com>
2020-12-11 01:36:58 -0800

    4 category added

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -3,8 +3,8 @@ import Movie from './Movie';
 
  const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=96c32a4771978ce6e86038f43c145982&page=1&include_adult=false&query=`;
 
- const TOP_RATED = `https://api.themoviedb.org/3/movie/top_rated?api_key=96c32a4771978ce6e86038f43c145982`;
  const POPULAR = `https://api.themoviedb.org/3/movie/popular?api_key=96c32a4771978ce6e86038f43c145982`;
+ const TOP_RATED = `https://api.themoviedb.org/3/movie/top_rated?api_key=96c32a4771978ce6e86038f43c145982`;
  const UPCOMING = `https://api.themoviedb.org/3/movie/upcoming?api_key=96c32a4771978ce6e86038f43c145982`;
  const NOW_PLAYING = `https://api.themoviedb.org/3/movie/now_playing?api_key=96c32a4771978ce6e86038f43c145982`;
 
@@ -14,6 +14,7 @@ export const SearchMovies = () => {
 
  const[movies, setMovies] = useState([]);
  const[searchTerm, setSearchTerm] = useState('');
+ const[dropdown, setDropdown] = useState('');
 
     useEffect(() =>{
         fetch(POPULAR)
@@ -42,6 +43,23 @@ export const SearchMovies = () => {
     const handleOnChange = (e) =>{
         setSearchTerm(e.target.value);
     };
+
+    const handleDropdownChange = (e) =>{    
+        setDropdown(e.target.value);
+     
+    }
+
+const handleDropdownSubmit = (e) =>{
+    e.preventDefault();
+    
+    fetch(`https://api.themoviedb.org/3/movie/${dropdown}?api_key=96c32a4771978ce6e86038f43c145982`)
+    .then((res) => res.json())
+    .then((data) =>{
+        // console.log(data);
+        setMovies(data.results);
+    });
+    
+    };
    
     return (
         <>
@@ -51,6 +69,16 @@ export const SearchMovies = () => {
             {/* <button
              className="button" type="submit">Search</button> */}
         </form> 
+        <form onSubmit={handleDropdownSubmit}>
+            <select value={dropdown}  onChange={handleDropdownChange}>
+             <option value= "popular">POPULAR</option>
+             <option value="top_rated">TOP RATED</option>
+             <option value="upcoming">UPCOMING</option>
+             <option value="now_playing">NOW PLAYING</option>
+            </select>
+            <button className="button" type="submit">Search</button>
+        </form> 
+
         <div className="movie-container">
             {movies.length > 0 && movies.map((movie) =>
                 <Movie key={movie.id} {...movie} />
