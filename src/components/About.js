31b0c3bc487dbbcd57235a0b89ef3d16eab39e13
Aya-Watanabe <Aya-Watanabe@users.noreply.github.com>
2020-12-14 04:10:59 -0800

    image changed

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const About = () => {
     return (
         <>
-        <h1>About</h1>
+        <h1 className="about-h1">About</h1>
         <div className="movie-container">
            
         </div>
--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -3,13 +3,15 @@ import {GlobalContext} from '../context/GlobalState';
 import WatchlistMovie from "./WatchlistMovie";
 
 
+
 export const Watchlist = () => {
   
     const {watchlist} = useContext(GlobalContext);
    
+
     return (
         <>
-        <h1>My Watch List</h1>
+        <h1 className="fav-h1">My Watch List</h1>
 
         {watchlist.length > 0 ? (
         <div className="movie-container">
@@ -19,9 +21,11 @@ export const Watchlist = () => {
             ))}
         </div>
             ) : (
-                <div>
-                    <img src="/../images/film_large.png"/>
-                    <h2>No movie in the list</h2>
+                <div className="no-fev-movie">
+                    <h2>No movie in the list... add some <i class="fas fa-video"></i></h2>
+                   {/* <img src="https://www.nicepng.com/png/detail/50-505017_png-file-svg-movie-ticket-svg.png" alt="Png File Svg - Movie Ticket Svg@nicepng.com" /> */}
+                   <img src="https://www.nicepng.com/png/detail/309-3099990_people-watching-a-movie-vector-people-watching-movie.png" alt="People Watching A Movie Vector - People Watching Movie Icon@nicepng.com"></img>
+                    
                 </div>
                 
         ) }
