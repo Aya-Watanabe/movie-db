f576ded424e4aa2550e5f6ff2dcf99e0827fe530
Aya-Watanabe <Aya-Watanabe@users.noreply.github.com>
2020-12-16 05:19:02 -0800

    about page edited

--- src/components/About.js
+++ src/components/About.js
@@ -2,16 +2,18 @@ import React from 'react'
 
 export const About = () => {
     return (
-        <>
-        <h1 className="about-h1">About Movie DB</h1>
-        <div className="movie-container">
-           <p>Movie DB is a Movie Database listing the movies based on popularity, rating, release date,now playing, or keywords. Browse for your favourite film, add it to the Watch List, and save it for the Watch Later list.</p>
-           <div>
-           <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
-           <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer"><img src ={`https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg`} alt="Logo" style={{height:"30px"}} /></a>
-           </div>
-        </div>
-       </>
+        <div className="about">
+            
+            <div class="overlay-text" id="overlay-text">
+                <h1 class="overlay-text-big">
+                About This Website
+                </h1>
+                <p>This is a Movie Database listing the movies based on popularity, rating, release date,now playing, or keywords. 
+                    <br />Browse for your favourite film, add it to the Watch List, and save it for the Watch Later list.</p>
+                <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+                <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer"><img src ={`https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg`} alt="Logo" style={{height:"30px"}} /></a>
+            </div>
+       </div>
     )
 }
 
